931cc054702b2752e4377630277461093241cfc3
Gary A. Stafford <garystafford@rochester.rr.com>
2015-01-27 00:01:26 -0500

    Enhancing contents of default index.html page

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,26 +10,11 @@
 #
 # Sample Usage:
 #
-class apache_example_config () inherits apache {
-  $str_node01 = '<!DOCTYPE html><html><body><h1>HAProxy Example</h1>
-    <p>Example of HAProxy reverse proxy, load-balanced web servers.<br/><br/>
-    docroot: /var/www/example</p></body></html>'
-
-  $str_default = '<!DOCTYPE html><html><body><h1>HAProxy Example</h1>
-    <p>Example of HAProxy reverse proxy, load-balanced web servers.<br/><br/>
-    docroot: /var/www/html</p></body></html>'
-
-  # vhost example
-  file { '/var/www/example': ensure => 'directory', } ->
-  file { '/var/www/example/index.html':
-    ensure  => 'present',
-    content => $str_node01,
-    mode    => '0644',
-  } ->
-  apache::vhost { 'node01.example.com':
-    port    => '80',
-    docroot => '/var/www/example',
-  }
+class apache_example_config {
+  $str_default = "<!DOCTYPE html><html><body><h1>HAProxy Example</h1>
+    <p>Example of using HAProxy as a reverse-proxy load-balancer,<br/>
+    with ${operatingsystem}-based Apache web servers.<br/><br/>
+    hostname: ${fqdn}<br/>ip address: ${network_eth1}</p></body></html>"
 
   # default directory
   file { '/var/www/html': ensure => 'directory', } ->
