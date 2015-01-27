9c07f5e896f63f126be155179353503e04113e66
Gary A. Stafford <garystafford@rochester.rr.com>
2015-01-27 15:49:56 -0500

    Updating index.html to use display static IP address (ipaddress_eth1).

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,7 +14,7 @@ class apache_example_config {
   $str_default = "<!DOCTYPE html><html><body><h1>HAProxy Example</h1>
     <p>Example of using HAProxy as a reverse-proxy load-balancer,<br/>
     with ${operatingsystem}-based Apache web servers.<br/><br/>
-    hostname: ${fqdn}<br/>ip address: ${network_eth1}</p></body></html>"
+    hostname: ${fqdn}<br/>ip address: ${ipaddress_eth1}</p></body></html>"
 
   # default directory
   file { '/var/www/html': ensure => 'directory', } ->
